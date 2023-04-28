b1d00ec3ea1f14f722168faffd6c218d60fab673
Teppi Le <teppipersonal@gmail.com>
2023-04-28 22:23:18 +0700

    subject page

--- interaction.js
+++ interaction.js
@@ -1,9 +1,15 @@
 const subjectsBtn = document.getElementById('subjectBtn');
+const subjectPage = document.getElementById('subjectPage');
 
 subjectsBtn.onclick = function() {
     document.body.classList.add("right-zoom-transition");
     setTimeout(() => {
         //whatever
+        subjectPage.classList.remove("hidden");
+    }, 600);
+
+    setTimeout(() => {
+        //whatever
         document.body.classList.remove("right-zoom-transition");
-    }, 1200);
+    }, 1800);
 }
